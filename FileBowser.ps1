6f8533a37a5e2d261686605075023d1c24b6c1b5
flxkrmr <8393480+flxkrmr@users.noreply.github.com>
2023-11-11 22:40:38 +0100

    Add classes and improve GUI update

--- FileBowser.ps1
+++ FileBowser.ps1
@@ -1,54 +1,136 @@
 class GuiLine {
+	[bool]$Dirty
+	
 	[string]$Text
 	[bool]$Selected
-	[bool]$Dirty
-}
 
+	GuiLine() {
+		$this.Selected = $false
+		$this.Dirty = $false
+	}
 
-function Write-BrowserGuiLine {
-	param (
-		$Text,
-		$Selected
-	)
-
-	if ($Selected) {
-		Write-Host $Text -BackgroundColor White -ForegroundColor Black
-	} else {
-		Write-Host $Text -BackgroundColor Black -ForegroundColor White
+	[string] Formatter() {
+		if ($this.Selected) {
+			#return "`e[31m"
+			return "`e[44m"
+		} else {
+			return "`e[0m"
+		}
 	}
 }
 
-function Build-BrowserGui {
-	$windowSize = (Get-Host).UI.RawUI.MaxWindowSize
-	
-}
+class Gui {
+	[GuiLine[]]$Lines
+	[int]$Height
+	[int]$Width
 
-function Write-BrowserGui {
-	param (
-		$SelectedLine,
-		$Items
-	)
-	
-	$currentLine = 0
+    Gui() {
+		$windowSize = (Get-Host).UI.RawUI.MaxWindowSize
 
-	clear
-	foreach ($child in $Items) {
-		if ($child -is [System.IO.DirectoryInfo]) {
-			$text = "`t" + $child.Name + "/"
-		} else {
-			$text = "`t" + $child.Name
+		$this.Width = $windowSize.Width
+		$this.Height = $windowSize.Height - 1
+
+		$this.Lines = [GuiLine[]]::new($this.Height)
+		for ($i = 0; $i -lt $this.Lines.Length; $i++) {
+			$this.Lines[$i] = [GuiLine]::new()
 		}
 
-		$selected = $currentLine -eq $SelectedLine
-		Write-BrowserGuiLine -Text $text -Selected $selected 	
+		$this.Lines[6].Selected = $true
+    }
 
-		$currentLine++
+	[void] SetLineText([int]$LineIndex, [string]$Text) {
+		$this.Lines[$LineIndex].Text = $Text
+		$this.Lines[$LineIndex].Dirty = $true
+	}
+
+	[void] SetLineSelected([int]$LineIndex) {
+		$this.Lines[$LineIndex].Selected = $true
+		$this.Lines[$LineIndex].Dirty = $true
+	}
+	
+	[void] SetAllLinesDeselected() {
+		foreach ($line in $this.Lines) {
+			if ($line.Selected) {
+				$line.Selected = $false
+				$line.Dirty = $true
+			}
+		}
+	}
+
+	[void] Draw() {
+		clear
+		for ($i = 0; $i -lt $this.Lines.Length; $i++) {
+			$line = $this.Lines[$i]
+			$this.DrawLine($i, $line.Text, $line.Formatter())
+			$line.Dirty = $false
+		}
+	}
+
+	[void] ReDraw() {
+		for ($i = 0; $i -lt $this.Lines.Length; $i++) {
+			$line = $this.Lines[$i]
+			if ($line.Dirty) {
+				$this.DrawLine($i, $line.Text, $line.Formatter())
+				$line.Dirty = $false
+			}
+		}
+	}
+
+	[void] DrawLine([int]$LineIndex, [string]$Text, [string]$Formatter) {
+		$l = $LineIndex + 1
+		Write-Host "`e[$l;1H`e[K|  $Formatter$Text`e[0m"
 	}
 }
 
-$keepGoing = $true
+function Main {
+	$gui = [Gui]::new()
 
-$selectedLine = 0
+	$files = Get-ChildItem
+	for ($i = 0; $i -lt $files.Length; $i++) {
+		$gui.SetLineText($i, $files[$i].Name)
+	}
+	
+	$cursorLine = 0
+	$gui.SetLineSelected($cursorLine)
+	
+	$gui.Draw()
+	
+	$keepGoing = $true
+	Do {
+		$key = [System.Console]::ReadKey()
+
+		switch($key.Key) {
+			UpArrow {
+				$cursorLine--
+				if ($cursorLine -lt 0) {
+					$cursorLine = $files.Length - 1
+				}
+			}
+			DownArrow {
+				$cursorLine++
+				if ($cursorLine -gt $files.Length - 1) {
+					$cursorLine = 0
+				}
+			}
+			Q {
+				$keepGoing = $false
+			}
+		}
+	
+		$gui.SetAllLinesDeselected()
+		$gui.SetLineSelected($cursorLine)
+
+		$gui.ReDraw()
+	} while ($keepGoing)
+
+	clear
+}
+
+Main
+
+Exit
+
+
 
 $children = Get-ChildItem
 Write-BrowserGui -Items $children -SelectedLine $selectedLine
